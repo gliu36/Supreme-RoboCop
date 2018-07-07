0142988b3eafa2e98671aa1f2e6c8caaa5fde023
Gerry <gerryliu36@gmail.com>
2018-07-06 21:30:34 -0500

    first

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup
+import re
+import json
+
+def findItem(category, keyWords):
+    url = "https://www.supremenewyork.com/mobile/#categories/"
+    uCLient = uReq(url)
+    page_html = uCLient.read()
+    uCLient.close()
+    soup = BeautifulSoup(page_html, "html.parser")
+    pattern = re.compile(r"var allCategoriesAndProducts = (.*?);")
+    script = soup.find("script", text=pattern)
+    items = pattern.search(script.text).group(1)
+    json_stringify = json.loads(items)
+    category = json_stringify["products_and_categories"][category]
+
+    #for item in category:
+
+
+
+    prettyPrint = json.dumps(category, indent=4)
+    return prettyPrint
+
+keyWords = ["Striped"]
+print(findItem("Tops/Sweaters", keyWords))
